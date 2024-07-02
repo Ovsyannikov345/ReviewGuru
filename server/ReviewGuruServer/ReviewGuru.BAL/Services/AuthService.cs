227d80ed766147534e146d9d2d5acf720f2dddd3
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-03 00:21:31 +0300

    feat: create auth service classes

---
+++ server/ReviewGuruServer/ReviewGuru.BAL/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using ReviewGuru.BLL.DTOs;
+using ReviewGuru.BLL.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.BLL.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<TokenDto> LoginAsync(LoginDto authData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LogoutAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenDto> RegisterAsync(RegisterDto userData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ server/ReviewGuruServer/ReviewGuru.BAL/Services/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using ReviewGuru.BLL.DTOs;
+using ReviewGuru.BLL.Services.IServices;
+using ReviewGuru.DAL.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.BLL.Services
+{
+    public class TokenService : ITokenService
+    {
+        public Task<TokenDto> CreateTokenAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenDto> RefreshTokenAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
