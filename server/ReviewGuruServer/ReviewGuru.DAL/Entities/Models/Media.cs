ce879fbae53495a482396486c417b842922e4e1d
8merka <8merka88@gmail.com>
2024-07-02 15:54:15 +0300

    init: new architecture and classes

---
+++ server/ReviewGuruServer/ReviewGuru.DAL/Entities/Models/Media.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.DAL.Entities.Models
+{
+    internal class MediaId
+    {
+    }
+}
---
+++ server/ReviewGuruServer/ReviewGuru.DAL/Entities/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.DAL.Entities.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int UserId { get; set; }
+        public int MediaId { get; set; }
+        public int Rating { get; set; }
+        public string UserReview { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
