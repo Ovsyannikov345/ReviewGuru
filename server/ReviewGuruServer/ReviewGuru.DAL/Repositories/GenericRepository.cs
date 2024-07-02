81e957b6c287fa424b29cc7c6256121dd0ed3c12
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-02 23:31:54 +0300

    feat: add generic repository

---
+++ server/ReviewGuruServer/ReviewGuru.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewGuru.DAL.Data;
+using ReviewGuru.DAL.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.DAL.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class, new()
+    {
+        private readonly ReviewGuruDbContext _context;
+
+        public GenericRepository(ReviewGuruDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _context.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task<List<TEntity>> GetListAsync(int pageNumber, int pageSize, Expression<Func<TEntity, bool>>? filter = null)
+        {
+            var entities = filter == null ? _context.Set<TEntity>() : _context.Set<TEntity>().Where(filter);
+
+            return await entities.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<int> DeleteAsync(TEntity entity)
+        {
+            _context.Remove(entity);
+
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ server/ReviewGuruServer/ReviewGuru.DAL/Repositories/IRepositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.DAL.Repositories.IRepositories
+{
+    public interface IGenericRepository<T> where T : class, new()
+    {
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter);
+
+        Task<List<T>> GetListAsync(int pageNumber, int pageSize, Expression<Func<T, bool>>? filter = null);
+
+        Task<T> AddAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<int> DeleteAsync(T entity);
+    }
+}
