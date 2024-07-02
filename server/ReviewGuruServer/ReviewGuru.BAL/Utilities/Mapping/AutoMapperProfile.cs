f5a02f42e892a5520c47ce4db5cd4f2eac957296
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-03 00:21:56 +0300

    feat: create auto mapper with profile for registration

---
+++ server/ReviewGuruServer/ReviewGuru.BAL/Utilities/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ReviewGuru.BLL.DTOs;
+using ReviewGuru.DAL.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.BLL.Utilities.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RegisterDto, User>();
+        }
+    }
+}
