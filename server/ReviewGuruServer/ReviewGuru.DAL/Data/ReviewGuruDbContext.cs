783b20b7db2fc71189e9555122f886633a9d802c
8merka <8merka88@gmail.com>
2024-07-02 16:28:19 +0300

    fix: add full set of tables

--- server/ReviewGuruServer/ReviewGuru.DAL/Data/ReviewGuruDbContext.cs
+++ server/ReviewGuruServer/ReviewGuru.DAL/Data/ReviewGuruDbContext.cs
@@ -6,7 +6,15 @@ namespace ReviewGuru.DAL.Data
     public class ReviewGuruDbContext : DbContext
     {
         public ReviewGuruDbContext(DbContextOptions<ReviewGuruDbContext> options) : base(options) { }
-        public DbSet<Author> Author { get; set; }
+        public DbSet<Author> Authors { get; set; }
         public DbSet<Media> Media { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<MediaAuthor> MediaAuthor { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
     }
 }
