083354e2bab69a8a36c85a30851f4bec1f84aede
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-03 00:21:13 +0300

    feat: add auth service interfaces

---
+++ server/ReviewGuruServer/ReviewGuru.BAL/Services/IServices/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using ReviewGuru.BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.BLL.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<TokenDto> LoginAsync(LoginDto authData);
+
+        Task<TokenDto> RegisterAsync(RegisterDto userData);
+
+        Task<bool> LogoutAsync(string refreshToken);
+    }
+}
---
+++ server/ReviewGuruServer/ReviewGuru.BAL/Services/IServices/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using ReviewGuru.BLL.DTOs;
+using ReviewGuru.DAL.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewGuru.BLL.Services.IServices
+{
+    public interface ITokenService
+    {
+        string GenerateToken(IEnumerable<Claim> claims);
+
+        Task<TokenDto> CreateTokenAsync(User user);
+
+        Task<TokenDto> RefreshTokenAsync(User user);
+    }
+}
