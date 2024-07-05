a201e04d0f5aca11d52923349ef23e875a266870
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-05 22:05:03 +0300

    feat: change locale to US

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import NavBar from './components/NavBar';
-import AppRouter from './router/AppRouter';
+import NavBar from "./components/NavBar";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
+    return (
+        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-us">
             <BrowserRouter>
                 <NavBar />
                 <AppRouter />
             </BrowserRouter>
         </LocalizationProvider>
-  );
+    );
 }
 
 export default App;
