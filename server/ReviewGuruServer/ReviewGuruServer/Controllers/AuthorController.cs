34bc0d7c082727c1292a655499aa1bcc0a35c076
8merka <8merka88@gmail.com>
2024-07-05 13:56:28 +0300

    feat: new controllers

---
+++ server/ReviewGuruServer/ReviewGuruServer/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReviewGuru.BLL.Services.IServices;
+
+namespace ReviewGuru.API.Controllers
+{
+    [Route("api/author")]
+    [ApiController]
+    public class AuthorController(IAuthorService authorService) : ControllerBase
+    {
+        private readonly IAuthorService _authorService = authorService;
+
+        [HttpGet]
+        [ActionName("GetAllAuthors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllAuthorsAsync(CancellationToken cancellationToken, int pageNumber = 1, int pageSize = 10)
+        {
+            var authors = await _authorService.GetAllAsync(pageNumber, pageSize, cancellationToken);
+            return Ok(authors.ToList());
+        }
+    }
+}
