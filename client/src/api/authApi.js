699a61c8153eaf59f963b24763fe2b7e51c17c64
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-08 01:44:16 +0300

    feat: add request for email verification

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -73,4 +73,27 @@ const sendRegisterRequest = async (userData) => {
     }
 };
 
-export { sendLoginRequest, sendLogoutRequest, sendRegisterRequest };
+const sendVerificationRequest = async (verificationToken) => {
+    try {
+        const response = await host.post("/Auth/verify", { verificationToken: verificationToken });
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (!error.response.data.statusCode) {
+                error.response.data = {
+                    statusCode: error.response.status,
+                    message: "Service is currently unavailable",
+                };
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { statusCode: 500, message: "Service is currently unavailable" } };
+        } else {
+            return { data: { statusCode: 400, message: "Error while creating request" } };
+        }
+    }
+};
+
+export { sendLoginRequest, sendLogoutRequest, sendRegisterRequest, sendVerificationRequest };
