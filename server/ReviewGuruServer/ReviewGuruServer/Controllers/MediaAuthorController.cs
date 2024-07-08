bcbbe2ad6e5644e3b787d023723f6c36265903c7
8merka <8merka88@gmail.com>
2024-07-08 10:40:49 +0300

    feat: add base controllers

--- server/ReviewGuruServer/ReviewGuruServer/Controllers/MediaAuthorController.cs
+++ server/ReviewGuruServer/ReviewGuruServer/Controllers/MediaAuthorController.cs
@@ -1,24 +1,59 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using ReviewGuru.BLL.Services.IServices;
-//using ReviewGuru.BLL.Utilities.Constants;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReviewGuru.BLL.DTOs;
+using ReviewGuru.BLL.Services.IServices;
+using ReviewGuru.BLL.Utilities.Constants;
 
-//namespace ReviewGuru.API.Controllers
-//{
-//    [Route("api/mediaAuthor")]
-//    [ApiController]
-//    public class MediaAuthorController(IMediaAuthorService mediaAuthorService) : ControllerBase
-//    {
-//        private readonly IMediaAuthorService _mediaAuthorService;
+namespace ReviewGuru.API.Controllers
+{
+    [Route("api/mediaAuthor")]
+    [ApiController]
+    public class MediaAuthorController(IMediaAuthorService mediaAuthorService) : ControllerBase
+    {
+        private readonly IMediaAuthorService _mediaAuthorService = mediaAuthorService;
 
-//        [HttpGet]
-//        [ActionName("GetAllMediaAuthors")]
-//        [ProducesResponseType(StatusCodes.Status200OK)]
-//        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        [ActionName("GetAllMediaAuthors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllMediaAuthorsAsync(CancellationToken cancellationToken, int pageNumber = Pagination.PageNumber, int pageSize = Pagination.PageSize)
+        {
+            var mediaAuthors = await _mediaAuthorService.GetAllAsync(pageNumber, pageSize, cancellationToken);
+            return Ok(mediaAuthors.ToList());
+        }
 
-//        public async Task<IActionResult> GetAllAuthorsAsync(CancellationToken cancellationToken, int pageNumber = Pagination.PageNumber, int pageSize = Pagination.PageSize)
-//        {
+        [HttpPost]
+        [ActionName("CreateMediaAuthor")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateMediaAuthorAsync([FromBody] MediaAuthorDTO mediaAuthorDTO, CancellationToken cancellationToken = default)
+        {
+            await _mediaAuthorService.CreateAsync(mediaAuthorDTO, cancellationToken);
+            return Created();
+        }
 
-//        }
-//    }
-//}
+        [HttpPut]
+        [ActionName("UpdateMediaAuthor")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateMediaAuthorAsync([FromBody] MediaAuthorDTO mediaAuthorToUpdateDTO, CancellationToken cancellationToken = default)
+        {
+            await _mediaAuthorService.UpdateAsync(mediaAuthorToUpdateDTO, cancellationToken);
+            return Ok(mediaAuthorToUpdateDTO);
+        }
+
+        [HttpDelete]
+        [ActionName("DeleteMediaAuthor")]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteMediaAuthorAsync([FromRoute] int id, CancellationToken cancellationToken = default)
+        {
+            await _mediaAuthorService.DeleteAsync(id, cancellationToken);
+            return Ok();
+        }
+    }
+
+}
---
+++ server/ReviewGuruServer/ReviewGuruServer/Controllers/MediaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReviewGuru.BLL.DTOs;
+using ReviewGuru.BLL.Services.IServices;
+using ReviewGuru.BLL.Utilities.Constants;
+
+namespace ReviewGuru.API.Controllers
+{
+    [Route("api/media")]
+    [ApiController]
+    public class MediaController(IMediaService mediaService) : ControllerBase
+    {
+        private readonly IMediaService _mediaService = mediaService;
+
+        [HttpGet]
+        [ActionName("GetAllMedia")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAllMediaAsync(CancellationToken cancellationToken, int pageNumber = Pagination.PageNumber, int pageSize = Pagination.PageSize)
+        {
+            var media = await _mediaService.GetAllAsync(pageNumber, pageSize, cancellationToken);
+            return Ok(media.ToList());
+        }
+
+        [HttpPost]
+        [ActionName("CreateMedia")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateMediaAsync([FromBody] MediaDTO mediaDTO, CancellationToken cancellationToken = default)
+        {
+            await _mediaService.CreateAsync(mediaDTO, cancellationToken);
+            return Created();
+        }
+
+        [HttpPut]
+        [ActionName("UpdateMedia")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateMediaAsync([FromBody] MediaDTO mediaToUpdateDTO, CancellationToken cancellationToken = default)
+        {
+            await _mediaService.UpdateAsync(mediaToUpdateDTO, cancellationToken);
+            return Ok(mediaToUpdateDTO);
+        }
+
+        [HttpDelete]
+        [ActionName("DeleteMedia")]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteMediaAsync([FromRoute] int id, CancellationToken cancellationToken = default)
+        {
+            await _mediaService.DeleteAsync(id, cancellationToken);
+            return Ok();
+        }
+    }
+
+}
