b40148a9e4d32ed82f3b1fed870e4497cfc5c6fa
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-10 20:37:19 +0300

    feat: add repository method for getting user with favorites list

--- server/ReviewGuruServer/ReviewGuru.DAL/Repositories/IRepositories/IUserRepository.cs
+++ server/ReviewGuruServer/ReviewGuru.DAL/Repositories/IRepositories/IUserRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,6 @@ namespace ReviewGuru.DAL.Repositories.IRepositories
 {
     public interface IUserRepository : IGenericRepository<User>
     {
+        public Task<User?> GetUserWithFavoritesAsync(Expression<Func<User, bool>> filter, CancellationToken cancellationToken = default);
     }
 }
