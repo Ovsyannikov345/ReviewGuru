0485c9b8c97648f1ed4d83e5104956f9833776d9
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-07-24 21:56:28 +0300

    feat: add endpoint for getting author list

--- server/ReviewGuruServer/ReviewGuru.BAL/Services/AuthorService.cs
+++ server/ReviewGuruServer/ReviewGuru.BAL/Services/AuthorService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ReviewGuru.BLL.DTOs;
 using ReviewGuru.BLL.Services.IServices;
+using ReviewGuru.BLL.Utilities.Constants;
 using ReviewGuru.DAL.Entities.Models;
 using ReviewGuru.DAL.Repositories;
 using ReviewGuru.DAL.Repositories.IRepositories;
@@ -8,13 +9,26 @@ using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReviewGuru.BLL.Services
 {
-    public class AuthorService(IGenericRepository<Author> genericRepository, IMapper mapper, ILogger logger) : IAuthorService
+    public class AuthorService(IAuthorRepository authorRepository, IMapper mapper, ILogger logger) : IAuthorService
     {
+        private readonly IAuthorRepository _authorRepository = authorRepository;
 
+        private readonly ILogger _logger = logger;
+
+        public async Task<IEnumerable<Author>> GetAuthorListAsync(
+            int pageNumber = Pagination.PageNumber,
+            int pageSize = Pagination.MaxPageSize,
+            CancellationToken cancellationToken = default)
+        {
+            _logger.Information($"Page {pageNumber} of authors has been returned");
+
+            return await _authorRepository.GetAllAsync(pageNumber, pageSize, cancellationToken: cancellationToken);
+        }
     }
 }
---
+++ server/ReviewGuruServer/ReviewGuruServer/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReviewGuru.BLL.Services;
+using ReviewGuru.BLL.Services.IServices;
+using ReviewGuru.BLL.Utilities.Constants;
+
+namespace ReviewGuru.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController(IAuthorService authorService) : ControllerBase
+    {
+        private readonly IAuthorService _authorService = authorService;
+
+        [HttpGet]
+        [ActionName("GetAllAuthors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllAuthorsAsync(CancellationToken cancellationToken = default)
+        {
+            var authors = await _authorService.GetAuthorListAsync(cancellationToken: cancellationToken);
+
+            return Ok(authors);
+        }
+    }
+}
